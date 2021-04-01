12dea919f4ea292835ff7639fc654c34602dee62
Niels Baptist <r0793266@student.thomasmore.be>
2021-04-01 20:50:06 +0200

    added mailing

--- app.js
+++ app.js
@@ -48,12 +48,7 @@ function hideAll(DivToShow) {
     $('#DivContact').hide();
     $('#DivProjects').hide();
     $('#DivCv').hide();
-    // if(DivToShow.attr('id') == "DivContact"){
-    //     $(DivToShow).show(600);
-    // }
-    // else{
-        $(DivToShow).show();
-    //}
+    $(DivToShow).show();
 }
 
 //button class toepassen/verwijderen
