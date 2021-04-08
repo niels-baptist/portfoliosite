7b38c35397a9b2d62d39163a1051192e1a491625
Niels Baptist <r0793266@student.thomasmore.be>
2021-04-08 18:14:46 +0200

    code cleanup

--- app.js
+++ app.js
@@ -2,47 +2,37 @@ $(document).ready(function() {
     hideAll($('#accordion'));
     resetClass($('#BtnAbout'));
 });
-
 $(window).on('load', function () {
     $('select').addClass('p-2 rounded');
   });
-
 $( "#BtnProjects" ).click(function() {
     hideAll($('#DivProjects'));
     resetClass($('#BtnProjects'));
 });
-
 $( "#BtnProjectsB" ).click(function() {
     hideAll($('#DivProjects'));
     resetClass($('#BtnProjects'));
 });
-
 $( "#BtnResumeA" ).click(function() {
     hideAll($('#DivCv'));
     resetClass($('#BtnCv'));
 });
-
 $( "#BtnAbout" ).click(function() {
     hideAll($('#accordion'))
     resetClass($('#BtnAbout'));
 });
-
 $( "#BtnContact" ).click(function() {
     hideAll($('#DivContact'));
     resetClass($('#BtnContact'));
 });
-
 $( "#BtnContactA" ).click(function() {
     hideAll($('#DivContact'));
     resetClass($('#BtnContact'));
 });
-
 $( "#BtnCv" ).click(function() {
     hideAll($('#DivCv'));
     resetClass($('#BtnCv'));
 });
-
-//divs verbergen/tonen
 function hideAll(DivToShow) {
     $('#accordion').hide();
     $('#DivContact').hide();
@@ -50,8 +40,6 @@ function hideAll(DivToShow) {
     $('#DivCv').hide();
     $(DivToShow).show();
 }
-
-//button class toepassen/verwijderen
 function resetClass(LinkToGiveClass) {
     $('#BtnAbout').removeClass('text-white bg-info');
     $('#BtnProjects').removeClass('text-white bg-info');
@@ -59,3 +47,6 @@ function resetClass(LinkToGiveClass) {
     $('#BtnCv').removeClass('text-white bg-info');
     $(LinkToGiveClass).addClass('text-white bg-info');
 }
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  }
